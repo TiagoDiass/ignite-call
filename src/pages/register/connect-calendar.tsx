da63811ce72b0e5f59934f028320a028a3492045
TiagoDiass <tiago.costadiasss@gmail.com>
2023-01-07 15:19:58 -0300

    feat(ConnectCalendar): add layout of ConnectCalendar

---
+++ src/pages/register/connect-calendar.tsx
@@ -0,0 +1,5 @@
+import { ConnectCalendar as ConnectCalendarTemplate } from '../../templates/ConnectCalendar/ConnectCalendar';
+
+export default function ConnectCalendar() {
+  return <ConnectCalendarTemplate />;
+}
---
+++ src/templates/ConnectCalendar/ConnectCalendar.tsx
@@ -0,0 +1,53 @@
+import { Button, Heading, MultiStep, Text } from '@ignite-ui/react';
+import { ArrowRight, Check, CheckCircle } from 'phosphor-react';
+import { useState } from 'react';
+
+import * as RegisterStyles from '../Register/Register.styles';
+import * as S from './ConnectCalendar.styles';
+
+export function ConnectCalendar() {
+  const [userConnectedCalendar, setUserConnectedCalendar] = useState(false);
+
+  const handleConnectCalendar = () => {
+    setUserConnectedCalendar(true);
+  };
+
+  return (
+    <RegisterStyles.Wrapper>
+      <RegisterStyles.Header>
+        <Heading>Conecte sua agenda!</Heading>
+
+        <Text>
+          Conecte o seu calendário para verificar automaticamente as horas
+          ocupadas e os novos eventos à medida em que são agendados.
+        </Text>
+
+        <MultiStep size={4} currentStep={2} />
+      </RegisterStyles.Header>
+
+      <S.ConnectBox>
+        <S.ConnectItem>
+          <Text>Google Agenda</Text>
+
+          {userConnectedCalendar ? (
+            <Button disabled>
+              Conectado <Check />
+            </Button>
+          ) : (
+            <Button
+              variant='secondary'
+              size='sm'
+              onClick={handleConnectCalendar}
+            >
+              Conectar <ArrowRight />
+            </Button>
+          )}
+        </S.ConnectItem>
+
+        <Button type='submit' disabled={!userConnectedCalendar}>
+          Próximo passo <ArrowRight />
+        </Button>
+      </S.ConnectBox>
+    </RegisterStyles.Wrapper>
+  );
+}
